d52e2f3f03334e5bc8a26a3696d8bdb798bac1b2
Giovanna.Ferreira <giovanna.ferreira@rckyinformatica.onmicrosoft.com>
2022-06-02 09:34:50 -0300

    accomplished the challenge

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,29 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+     const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+     };
+    setTasks(oldState => [...oldState, data])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updateTasks = tasks.map(task => ({...task}))
+    
+    const foundItem = updateTasks.find(item => item.id === id)
+
+    if(!foundItem)
+    return; 
+
+    foundItem.done = !foundItem.done
+    setTasks(updateTasks)
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks((oldState) => oldState.filter((tasks) => tasks.id != id))
+   
   }
 
   return (
@@ -29,7 +43,7 @@ export function Home() {
       <TasksList 
         tasks={tasks} 
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={(id) => handleRemoveTask(id)} 
       />
     </View>
   )
